45a381ad4a68ac720f38ffdd0ef84f184ac36695
chetannn-github <chetan.rajawat25@gmail.com>
2025-07-20 14:25:13 +0530

    style - made navbar responsive.

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -1,26 +1,29 @@
 import React from 'react'
 import { LOGO_URL } from '../utils/constant'
 import { Link } from 'react-router'
+import { Menu } from 'lucide-react'
 
 function NavBar() {
   return (
-    <div className='flex justify-center gap-20  w-full py-[18px] border-b-[0.5px] border-[#00000018]'>
+    <div className='flex justify-between gap-20  w-full py-[18px] border-b-[0.5px] border-[#00000018]'>
 
       <div className='flex  gap-8 items-center cursor-pointer'>
-        <Link to={'/'}><img src={LOGO_URL} className='h-[20px] '/></Link>
+        <Link to={'/'}><img src={LOGO_URL} className='h-[] w-[100px] object-contains'/></Link>
         
-        <div className='hover:text-primary text-[14px]'>Invest</div> 
-        <div className='hover:text-primary text-[14px]'>Trade</div>
-        <div className='hover:text-primary text-[14px]'>Spotlight</div>
-        <div className='hover:text-primary text-[14px]'>Ease API</div>
-        <div className='hover:text-primary text-[14px]'>Pricing</div>
-        <div className='hover:text-primary text-[14px]'>Learn</div>
+        <div className='hover:text-primary text-[14px] hidden lg:flex'>Invest</div> 
+        <div className='hover:text-primary text-[14px] hidden lg:flex'>Trade</div>
+        <div className='hover:text-primary text-[14px] hidden lg:flex'>Spotlight</div>
+        <div className='hover:text-primary text-[14px] hidden lg:flex'>Ease API</div>
+        <div className='hover:text-primary text-[14px] hidden lg:flex'>Pricing</div>
+        <div className='hover:text-primary text-[14px] hidden lg:flex'>Learn</div>
       </div>
 
       <div className='flex gap-5' >
-        <div className='rounded-xl border-2 text-secondary px-[24px] py-[10px] text-sm font-bold flex justify-center items-center'>Login</div>
-        <div className='rounded-xl bg-secondary text-white px-[24px] py-[10px] text-sm flex justify-center items-center' >Create Account</div>
+        <div className='rounded-xl border-2 text-secondary px-4 py-3 text-sm font-bold  justify-center items-center hidden lg:flex'>Login</div>
+        <div className='rounded-xl bg-secondary text-white px-4 py-3 text-sm justify-center items-center hidden lg:flex' >Create Account</div>
 
+
+        <div className='flex justify-center items-center lg:hidden'><Menu color='#191442' /></div>
       </div>
     
     </div>
