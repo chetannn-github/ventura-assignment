40e32ec0083deb9859be556251ebcecf7fd00bcd
chetannn-github <chetan.rajawat25@gmail.com>
2025-07-20 16:32:35 +0530

    style:  adjust text sizes in IPOCompanyList component for improved readability

--- src/components/IPO/IPOCompanyList.jsx
+++ src/components/IPO/IPOCompanyList.jsx
@@ -20,7 +20,7 @@ function IPOCompanyList({listingType}) {
         <div className='relative flex flex-col w-full py-5'>
             <div className='flex bg-[#f8f8f8]' >
                 {["Company","Issue size","Price range","Min. Amount"].map((val, idx)=>
-                    <div key= {val} className='relative w-1/4 flex justify-center text-sm' >
+                    <div key= {val} className='relative w-1/4 flex justify-center text-xs md:text-sm' >
                         {val}
                     </div>
                 )}
@@ -36,7 +36,7 @@ function IPOCompanyList({listingType}) {
                         <img className='h-5' src={val?.companyLogo}></img>
                     </div>
                         <div className='flex flex-col'>
-                            <h4 className='font-black text-sm'>{val?.name}</h4>
+                            <h4 className='font-black text-xs md:text-sm'>{val?.name}</h4>
                             <p className='text-xs text-[#0000008f]'>{val?.date}</p>
                         </div>
                    
@@ -44,17 +44,17 @@ function IPOCompanyList({listingType}) {
 
 
 
-                <div className='w-1/4  justify-center flex font-black text-sm'>{val?.issueSize}</div>
-                <div className='w-1/4  justify-center flex font-black text-sm'>{val?.priceBand}</div>
+                <div className='w-1/4  justify-center flex font-black md:text-sm text-xs'>{val?.issueSize}</div>
+                <div className='w-1/4  justify-center flex font-black md:text-sm text-xs'>{val?.priceBand}</div>
 
                 <div className='flex-col w-1/4 flex items-center'>
-                    <h4 className='font-black text-sm'>{val?.lotValue}</h4>
+                    <h4 className='font-black md:text-sm text-xs'>{val?.lotValue}</h4>
                     <p className='text-xs text-[#0000008f]'>{val?.lotSize}</p>
                 </div>
                
                 </div> 
 
-                <div className='bg-[#ffeee5] w-full flex items-center justify-center h-10 font-bold rounded-xl'>
+                <div className='bg-[#ffeee5] w-full flex items-center justify-center h-10 font-bold rounded-xl md:text-sm text-xs'>
                     {listingType === "Closed" ? val?.subscribed : `Listed at ${val?.listedAt} with  ${val?.gain} gains.`}
                 </div>
 
