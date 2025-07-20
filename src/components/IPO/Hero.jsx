6ed8130c0f21daaf8763d641411297c62eb16d9f
chetannn-github <chetan.rajawat25@gmail.com>
2025-07-20 16:47:17 +0530

    style: enhance layout and spacing in Hero component for improved readability

--- src/components/IPO/Hero.jsx
+++ src/components/IPO/Hero.jsx
@@ -15,16 +15,16 @@ function Hero() {
     <div className='relative flex w-full flex-col-reverse h-[100vh] md:flex-row md:justify-between md:h-[50vh]'>
 
 
-        <div className='relative flex flex-col justify-around h-1/2 md:h-full md:w-2/3'>
+        <div className='relative flex flex-col justify-around h-1/2 md:h-full md:w-2/3 gap-4'>
             <div className='flex flex-col gap-5'>
                 <h1 className='text-5xl font-black '>IPO - <span className='text-primary'>Intial Public <br /> Offerings</span></h1>
                 <p className='text-xl'>Identify the right IPOs to invest in with our research notes</p>
             </div>
 
-            <div className='relative w-full flex flex-col gap-2 '>
-                <h3 className='text-sm font-black'>Open an account</h3>
+            <div className='relative w-full flex flex-col gap-1 '>
+                <h3 className='text-md font-black'>Open an account</h3>
 
-                <div className='flex bg-[#f8f8f8] h-19 items-center gap-4 p-2 rounded-xl justify-between w-4/5 md:w-md '>
+                <div className='flex bg-[#f8f8f8] h-19 items-center gap-4 p-2 rounded-xl justify-between w-fit  md:w-md '>
                     <div className='flex gap-4 w-full'>
                         <h1>+91</h1>
                         <input 
@@ -34,7 +34,7 @@ function Hero() {
                         value={mobileNumber} 
                         onChange={handleChangeMobileNum}/>
                    </div> 
-                   <div className='bg-primary h-10/12 w-1/4 flex items-center justify-center rounded-xl p-3'><ChevronRight color={"white"} /></div>
+                   <div className='bg-primary h-10/12 w-[60px] flex items-center justify-center rounded-xl p-3'><ChevronRight color={"white"} /></div>
 
                 </div>
             </div>
