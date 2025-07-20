6e706d39aa63f39f053542b8ed517625fbf455bd
chetannn-github <chetan.rajawat25@gmail.com>
2025-07-20 08:31:46 +0530

    fix: correct width percentage in IPO component for proper layout

--- src/Pages/IPO/IPO.jsx
+++ src/Pages/IPO/IPO.jsx
@@ -6,7 +6,7 @@ import BreadCrumb from '../../components/BreadCrumb'
 function IPO() {
     let currPath = useLocation();
     return (
-    <div className='relative w-[100] h-[fit] flex flex-col items-center'> 
+    <div className='relative w-[100%] h-[fit] flex flex-col items-center'> 
         <NavBar/>
         <div className='relative px-45 w-full'>
           <BreadCrumb path={currPath.pathname}/>
